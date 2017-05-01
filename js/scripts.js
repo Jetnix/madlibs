8d2895a5d78e3a26d113591948f402ff8c454b6e
Chase Grijalva and Maria Del Castillo <maria@>
2017-05-01 11:00:21 -0700

    test

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,11 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
-    var name1Input = $("input#name1").val();
-    var name2Input = $("input#name2").val();
-    var verb1Input= $("input#verb1").val();
-    var verb2Input = $("input#verb2").val();
-    var bodypartInput = $("input#bodypart").val();
+    var blanks = ["name1", "name2", "verb1", "verb2", "bodypart"];
 
-    $(".name1").text(name1Input);
-    $(".name2").text(name2Input);
-    $(".verb1").text(verb1Input);
-    $(".verb2").text(verb2Input);
-    $(".bodypart").text(bodypartInput);
+    blanks.forEach(function(blank) {
+      var inputs =$("input#" + blank).val();
+      $("." + blank).text(inputs);
+    });
 
     $("#story").show();
 
